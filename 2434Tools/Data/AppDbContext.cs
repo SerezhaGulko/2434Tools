5560a38374995ed60aea5574b492176dc9786a45
RaisStepanov <stepr999@gmail.com>
2021-03-16 19:50:34 +0700

    Added SQL database support

---
+++ 2434Tools/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace _2434Tools.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) :
+            base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ 2434Tools/DbMigration.cs
@@ -0,0 +1,21 @@
+﻿using _2434Tools.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _2434Tools
+{
+    public static class DbMigration
+    {
+        public static IWebHost MigrateDatabase(this IWebHost webHost)
+        {
+            using(var scope = webHost.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.Migrate();
+            }
+
+            return webHost;
+        }
+    }
+}
