24694b2f89c168cd2dd1e8dff629a9a058b686d9
RaisStepanov <stepr999@gmail.com>
2021-03-17 22:27:23 +0700

    Completed Task #7536

--- 2434Tools/Program.cs
+++ 2434Tools/Program.cs
@@ -1,11 +1,6 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace _2434Tools
 {
@@ -13,14 +8,12 @@ namespace _2434Tools
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Program.BuildWebHost(args).MigrateDatabase().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
