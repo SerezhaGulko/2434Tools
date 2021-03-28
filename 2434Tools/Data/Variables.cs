514c04e326f5b3be91956c68a25d1b200c93899e
RaisStepanov <stepr999@gmail.com>
2021-03-28 19:58:04 +0700

    Task #7785

---
+++ 2434Tools/Data/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2434Tools.Data
+{
+    public static class Variables
+    {
+        public const float UpdateLiverInterval  = 720   ;
+        public const Int32 UpdateFeedBatch      = 6     ;
+        public const float UpdateFeedInterval   = 1     ;
+        public const float UpdateVideoInterval  = 1     ;
+        public const String API_KEY             = ""    ;
+    }
+}
