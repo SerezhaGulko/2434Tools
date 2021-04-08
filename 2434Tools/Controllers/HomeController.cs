fdd741f09e9fc66d52d7355d64d9f20d7a47d0a8
RaisStepanov <stepr999@gmail.com>
2021-04-08 21:37:26 +0700

    Add controller and page for live streams lists

--- 2434Tools/Controllers/HomeController.cs
+++ 2434Tools/Controllers/HomeController.cs
@@ -9,67 +9,35 @@ using System.Threading.Tasks;
 
 using Google.Apis.YouTube.v3;
 using Google.Apis.Services;
+using _2434Tools.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace _2434Tools.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            this._context = context;
         }
 
         public async Task<IActionResult> Index()
         {
-            const String API_KEY        = "API_KEY";
-            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
-            {
-                ApiKey = API_KEY,
-                ApplicationName = this.GetType().ToString()
-            });
-
-            // ChannelList Example
-            /*
-            const String ChannelId1     = "UCHVXbQzkl3rDfsXWo8xi2qw";
-            const String ChannelId2     = "UCt5-0i4AVHXaWJrL8Wql3mw";
-            var channel_request = youtubeService.Channels.List("snippet,statistics");
-            channel_request.Id = ChannelId1 + "," + ChannelId2;
-            channel_request.MaxResults = 50;
-            var resp = await channel_request.ExecuteAsync();
-            foreach(var channel in resp.Items)
-            {
-                channel.Snippet.Thumbnails;
-                channel.Snippet.Title;
-                channel.Statistics.SubscriberCount;
-                channel.Statistics.VideoCount;
-                channel.Statistics.ViewCount;
-            }
-            ViewData["resp"] = resp;*/
-
-            /*
-            // PlayList list 
-            const string ChannelId = "UC1uv2Oq6kNxgATlCiez59hw";
-            string uploadsPlaylistId = null;
-            // Get Uploads playlist id
-            var channelRequest = youtubeService.Channels.List("contentDetails");
-            channelRequest.Id = ChannelId;
-            var resp = await channelRequest.ExecuteAsync();
-            foreach(var channel in resp.Items) { uploadsPlaylistId = channel.ContentDetails.RelatedPlaylists.Uploads; }
-
-            var playlistRequest = youtubeService.PlaylistItems.List("contentDetails,snippet");
-            playlistRequest.PlaylistId = uploadsPlaylistId;
-            playlistRequest.MaxResults = 50;
-            var respPlaylist = await playlistRequest.ExecuteAsync();
-            foreach(var playlistItem in respPlaylist.Items)
-            {
-                //playlistItem.ContentDetails.VideoId;
-                //playlistItem.Snippet.Thumbnails
-                //playlistItem.Snippet.Title
-            }
-            ViewData["resp"] = respPlaylist;
-            */
+            DateTime since = DateTime.UtcNow - new TimeSpan(12, 0, 0);
+            var allVideos = await _context.Videos.Where(_v =>
+                _v.Status == VideoStatus.Live
+                || _v.Status == VideoStatus.Upcoming
+                || (_v.Status == VideoStatus.Finished && _v.Published > since)
+                )
+                .Include(v => v.Creator)
+                .ToListAsync();
+            ViewBag.Live = allVideos.Where(v => v.Status == VideoStatus.Live).OrderByDescending(v => v.LiveStartTime).ToList();
+            ViewBag.Upcoming = allVideos.Where(v => v.Status == VideoStatus.Upcoming).OrderBy(v => v.LiveStartTime).ToList();
+            ViewBag.New = allVideos.Where(v => v.Status == VideoStatus.Finished).OrderByDescending(v => v.Published).ToList();
             return View();
         }
 
